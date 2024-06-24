fec4e9a9311345232b78d6d91d98f2690134a4cf
Utkarsh Agrawal <utkarshagrawal@Utkarshs-MacBook-Pro-2.local>
2024-06-24 13:40:16 +0530

    init changes

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-  return {'data': {'name' : 'Utkarsh'}}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+
+  # only gets 10 published blogs
+  if published:
+    return {'data': f'{limit} published blogs from the db'}
+  else:
+    return {'data': f'{limit} blogs from the db'}
 
 @app.get('/about')
 def about():
   return {'data': 'about page'}
+
+class Blog(BaseModel):
+  title: str
+  body: str
+  published: Optional[bool]
+
+@app.post('/blog')
+def create(blog: Blog):
+  return blog
